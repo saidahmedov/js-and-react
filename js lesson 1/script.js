86ddf526f2d989194489dbc05086af875f65066e
Said <89637007721@mail.ru>
2021-09-06 13:10:24 +0300

    изменил файл js

--- js lesson 1/script.js
+++ js lesson 1/script.js
@@ -1,2 +1,16 @@
-console.log(2);
-alert('hello world')
+const number0Films = +prompt('Сколько Фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: number0Films,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних фильмов?', ''),
+      b = prompt('Насколько оцените его?', ''),
+      c = prompt('Один из последних фильмов?', ''),
+      d = prompt('Насколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
